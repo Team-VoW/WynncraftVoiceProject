5ff2ef3ea5d163e9f3cd53b70a14aa6df3eaeef4
bentechy <sendbenspam@yahoo.co.uk>
2021-02-25 15:06:37 +0000

    set up some initial mod stuff

---
+++ Basemod/src/main/java/com/wynnvp/wynncraftvp/ModCore.java
@@ -0,0 +1,31 @@
+package com.wynnvp.wynncraftvp;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+        modid = ModCore.MODID,
+        name = ModCore.NAME,
+        version = ModCore.VERSION
+)
+public class ModCore {
+    public static final String MODID = "wynncraftvp";
+    public static final String NAME = "Wynncraft Voice Project";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
