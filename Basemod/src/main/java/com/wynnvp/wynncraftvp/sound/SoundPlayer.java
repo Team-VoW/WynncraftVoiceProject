0407dc9626d3fe7a8cd4f43bce04651e766b00be
Shady <honza.stech@gmail.com>
2022-03-12 00:42:38 +0100

    Add a new method to send a HTTP POST request to our servers.
    This request will contain all information about any dialogue line, that doesn't have any audio file associated.
    I find it a bit hard to move around in the code as I'm not the one who wrote it, so I left a comment for you with clear instructions on what to do to make this work.
    The server-side part is fully functional now, it's just missing some tools to work with the saved reports.

--- Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundPlayer.java
+++ Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundPlayer.java
@@ -7,11 +7,18 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
-
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SoundPlayer {
     private int delay = 60;
+    private final String apiKey = "testing"; //NOTE: this must be changed before compiling
     private ArrayList<String> latestSoundPlayed;
 
     public SoundPlayer() {
@@ -24,6 +31,12 @@ public class SoundPlayer {
         SoundsHandler soundsHandler = modCore.soundsHandler;
         if (!soundsHandler.sounds.containsKey(line)) {
             System.out.println("Does not contain line: " + line);
+			if (reportUnvoicedLine(line)) {
+				System.out.println("Anonymous unvoiced line report has been sent to our servers. Don't worry, this doesn't contain any sensitive information, only the line that didn't have any audio file associated and your coordinates in Wynncraft.");
+			}
+			else {
+				System.out.println("An report of unvoiced line couldn't be sent. Error code should be right above this message.");
+			}
             return;
         }
         if (isOnCoolDown(line)) {
@@ -69,5 +82,37 @@ public class SoundPlayer {
         latestSoundPlayed.clear();
     }
 
+	private boolean reportUnvoicedLine(String chatMessage) throws IOException {
+		//TODO Find a way to fill these variables in an elegant way.
+		//I didn't dig through the code that much, but you could probably make a class that would take care of all the formating
+		//Something like ChatMessageFormator, which would have a method into which you'd pass the whole chat message, that should trigger a dialogue
+		//And after that, you could use methods like extractPlayerName(), extractNpcName(), extractDialogue() etc.
+		
+		String npcName = "";
+		String dialogue = "";
+		String fullLine = chatMessage;
+		int CoordX = 0;
+		int CoordY = 0;
+		int CoordZ = 0;
+		
+		URL urlObj = new URL("https://voicesofwynn.com/api/unvoiced-line-report/new?apiKey=" + apiKey);
+		HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
+		connection.setRequestMethod("GET");
+        connection.setRequestProperty("User-Agent", "VoicesOfWynnModClient");
+		connection.setDoOutput(true);
+		DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+		String urlPostParameters = "npc=" + npcName + "&dialogue=" + dialogue + "&full=" + fullLine + "&x=" + CoordX + "&y=" + CoordY + "&z=" + CoordZ + "&apiKey=testing"; 
+		outputStream.writeBytes(urlPostParameters); 
+		outputStream.flush();
+		outputStream.close();
+		Integer responseCode = connection.getResponseCode();
+		if (!(responseCode >= 200 && responseCode < 300)) {
+			System.out.println("HTTP response Code : " + responseCode);
+			return false;
+		}
+		System.out.println("HTTP response Code : " + responseCode);
+        
+		return true;
+	}
 
 }
