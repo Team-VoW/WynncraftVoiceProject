e3d06ac5cc0fd731a27183a634bccba5c7de7c6e
kmaxiyt <esst936@gmail.com>
2022-05-27 21:27:03 +0200

    Added infestid pit

--- Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundsHandler.java
+++ Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundsHandler.java
@@ -4578,6 +4578,15 @@ public class SoundsHandler {
         addSound("[1/1] Corrupted Garoth: No matter what you try, the fhwisbk4 stand no chance of surviving. I will track down every last one of fue: if it's the last thing I do. Admit it, you've lost.", "cls-corruptedgaroth-5", true);
         addSound("[1/1] Garoth: Did you not learn to not put your hand in a fire for the second time? Fool, prepare yourself for Gaia's most powerful force - fire!", "cls-corruptedgaroth-6", true);
 
+        //infestid pit
+        addSound("[1/2] ???: Oh, it seems I have a guest. You're just in time for dinner... hehe..", "infestidpit-arakadicus-1", true);
+        addSound("[1/3] Hanged Man: Ah- Adventurer!... I b- beg you.. do not c- continue on your quest...", "infestidpit-hangedman-1", true);
+        addSound("[2/3] Hanged Man: You might believe you c- can fight t- this monster... b- but you can't...", "infestidpit-hangedman-2", true);
+        addSound("[3/3] Hanged Man: Arakadicus is.. is a coward... it traps you and f- feeds you to its... children...", "infestidpit-hangedman-3", true);
+        addSound("[3/3] Hanged Man: Arakadicus is.. is a coward... is.. is a coward... it traps you and c- corrupts...", "infestidpit-hangedman-4", true);
+        addSound("[1/1] Arakadicus: Don't mind our other guest back there. He had such a bitter taste. However, my babies think you look delicious... hehehe...", "infestidpit-arakadicus-2", true);
+        addSound("[1/1] Arakadicus: Ah, the main course has arrived. You look so good, I think I'll have a taste of you myself!", "infestidpit-arakadicus-3", true);
+
 
 
 
