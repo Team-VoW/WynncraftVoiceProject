4fe3dbf043529de059d502b5bdbf3792c78227db
kmaxiyt <esst936@gmail.com>
2022-06-16 14:25:55 +0200

    Fixed overwritten methods in Soundhandler

--- Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundsHandler.java
+++ Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundsHandler.java
@@ -6,14 +6,22 @@ import net.minecraft.util.SoundEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import static com.wynnvp.wynncraftvp.utils.LineFormatter.formatToLineData;
 
 public class SoundsHandler {
-    public HashMap<String, CustomSoundClass> sounds;
+
+    //private final List<SoundObject> sounds;
+    private final HashMap<String, SoundObject> sounds;
+    private final Set<String> npcNames;
 
     public SoundsHandler() {
         sounds = new HashMap<>();
+        npcNames = new HashSet<>();
+        //sounds = new ArrayList<>();
         registerSounds();
     }
 
@@ -25,9 +33,50 @@ public class SoundsHandler {
         return event;
     }
 
-    private void addSound(String message, String name, boolean movingSound) {
-        message = formatToLineData(message).getSoundLine();
-        sounds.put(message, new CustomSoundClass(registerSound(name), movingSound));
+    /**
+     * Method to add the sounds to the system
+     * @param message Identification message
+     * @param id Identification sound
+     * @param movingSound True if the sound moves with the player,
+     *                    otherwise it will move with the ArmorStand
+     * */
+    private void addSound(String message, String id, boolean movingSound) {
+        LineData lineData = formatToLineData(message);
+        npcNames.add(lineData.getNPCName());
+        message = lineData.getSoundLine();
+        sounds.put(message, new SoundObject(lineData.getNPCName(), id, new CustomSoundClass(registerSound(id), movingSound)));
+    }
+
+    public boolean containsName(String rawName) {
+        return npcNames.contains(rawName);
+    }
+
+    public Optional<SoundObject> get(String message) {
+        return Optional.ofNullable(sounds.get(message));
+    }
+
+    public String findNPCName(String id) {
+        return sounds.values().stream()
+                .filter(soundObject -> soundObject.getId().equalsIgnoreCase(id))
+                .map(SoundObject::getNpcName)
+                .findAny()
+                .orElse("");
+    }
+
+    public String getNPCName(String quest) {
+        return sounds.entrySet().stream().filter(entry -> entry.getValue().getId().contains("-") &&
+                        entry.getValue().getId().contains(quest)
+                        && entry.getKey().contains("???:")).map(map ->
+                        map.getValue().getNpcName())
+                .findAny().orElse(null);
+    }
+
+    private void addName(String message, String name) {
+        String npcName = getNameForMessage(message);
+        if (npcName.isEmpty()) {
+            npcName = getNameForId(name);
+        }
+        npcNames.add(npcName);
     }
 
 
@@ -4999,6 +5048,21 @@ public class SoundsHandler {
 
 
 
+    public static String getNameForMessage(String message) {
+        String split = message.split(": ")[0];
+        return split.trim().toLowerCase().replaceAll("[^a-zA-Z0-9]", "").replaceAll("[0-9]", "");
+    }
+
+    public static String getNameForId(String name) {
+        String id = "???";
+        if (name.contains("-")) {
+            String[] args = name.split("-");
+            id = args[1];
+        } else if (name.contains("talkingmushroom")) {
+            id = "talkingmushroom";
+        }
+        return id;
+    }
 
 
 
