c42bc72348fd1497057450e1f379089e73ed7d2f
nullTheCoder <stepan.gaidukevich@gmail.com>
2022-10-01 19:39:54 +0300

    Fixes

--- 1.18/src/main/java/com/wynnvp/wynncraftvp/events/mixins/MixinChatListener.java
+++ 1.18/src/main/java/com/wynnvp/wynncraftvp/events/mixins/MixinChatListener.java
@@ -1,22 +1,19 @@
 package com.wynnvp.wynncraftvp.events.mixins;
 
 import com.wynnvp.wynncraftvp.events.ReceiveChatEvent;
-import net.minecraft.network.PacketByteBuf;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
-import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayPacketListener.class)
+@Mixin(ClientPlayNetworkHandler.class)
 public class MixinChatListener {
 
     @Inject(at = @At("RETURN"), method = "onGameMessage")
-    public void onGameMessage(GameMessageS2CPacket par1, CallbackInfo ci) {
+    public void onMessage(GameMessageS2CPacket par1, CallbackInfo ci) {
         String message = par1.getMessage().asString();
         if (!message.startsWith("§5[Voices of wynn]§r")) {
             ReceiveChatEvent.receivedChat(message);
