8c9f217506af758d22aa66252ed6ca29cac04f37
kmaxiyt <esst936@gmail.com>
2022-10-15 19:45:02 +0200

    Allow numbers in names to be received

--- 1.18/src/main/java/com/wynnvp/wynncraftvp/sound/line/LineData.java
+++ 1.18/src/main/java/com/wynnvp/wynncraftvp/sound/line/LineData.java
@@ -24,7 +24,10 @@ public class LineData {
 
     public String getNPCName() {
         String[] split = realLine.split(": ");
-        return split[0].trim().toLowerCase().replaceAll("[^a-zA-Z?]", "");
+        String name = split[0];
+        name = name.substring(name.indexOf("]") + 1);
+
+        return name.trim().toLowerCase().replaceAll("[^a-z\\d?]", "");
     }
 
 }
