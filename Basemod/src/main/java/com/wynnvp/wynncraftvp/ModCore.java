ca88a1693b0899da05e356828ff8c21d3482ab28
kmaxii <esst936@gmail.com>
2023-04-16 15:59:55 +0200

    Updated to 1.4

--- Basemod/src/main/java/com/wynnvp/wynncraftvp/ModCore.java
+++ Basemod/src/main/java/com/wynnvp/wynncraftvp/ModCore.java
@@ -12,7 +12,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
@@ -28,7 +27,7 @@ public class ModCore {
 
     public static final String MODID = "wynnvp";
     public static final String NAME = "Voices of wynn";
-    public static final String VERSION = "1.3.2"; // Don't forget to edit this
+    public static final String VERSION = "1.4"; // Don't forget to edit this
     public static boolean inServer = false;
     public static boolean inLiveWynnServer = false;
     public SoundsHandler soundsHandler;
@@ -36,11 +35,9 @@ public class ModCore {
     public SoundPlayer soundPlayer;
     public SoundController controller;
 
-    private static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        logger = event.getModLog();
         ConfigHandler.registerConfig(event);
     }
 
