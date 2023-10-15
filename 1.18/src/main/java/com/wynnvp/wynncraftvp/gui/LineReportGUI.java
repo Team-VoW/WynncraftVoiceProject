6bb7e0640e8ba4e57fffe6ef5eaf36afeda0c26c
kmaxii <esst936@gmail.com>
2023-10-15 17:48:24 +0200

    Made line report gui look okay again as the buttons got dragged out

--- 1.18/src/main/java/com/wynnvp/wynncraftvp/gui/LineReportGUI.java
+++ 1.18/src/main/java/com/wynnvp/wynncraftvp/gui/LineReportGUI.java
@@ -38,13 +38,13 @@ public class LineReportGUI extends LightweightGuiDescription {
     }
 
     private void AddText(WGridPanel root) {
-        root.add(new WLabel(Component.literal("VOICES OF WYNN")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 1);
-        root.add(new WLabel(Component.literal("Would you like your game to report unvoiced dialogues")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 2);
-        root.add(new WLabel(Component.literal("encountered while playing Wynncraft to improve")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 3);
-        root.add(new WLabel(Component.literal("Voices of Wynn?")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 4);
-        root.add(new WLabel(Component.literal("With full report, your nickname will be sent with the")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 6);
-        root.add(new WLabel(Component.literal("report, so that we could contact you in case we")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 7);
-        root.add(new WLabel(Component.literal("have questions about it.")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 8);
+        root.add(new WLabel(Component.literal("§5VOICES OF WYNN")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 1);
+        root.add(new WLabel(Component.literal("§fWould you like your game to report unvoiced dialogues")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 2);
+        root.add(new WLabel(Component.literal("§fencountered while playing Wynncraft to improve")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 3);
+        root.add(new WLabel(Component.literal("§fVoices of Wynn?")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 4);
+        root.add(new WLabel(Component.literal("§fWith full report, your nickname will be sent with the")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 6);
+        root.add(new WLabel(Component.literal("§freport, so that we could contact you in case we")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 7);
+        root.add(new WLabel(Component.literal("§fhave questions about it.")).setHorizontalAlignment(HorizontalAlignment.CENTER), 8, 8);
     }
 
 
@@ -65,9 +65,9 @@ public class LineReportGUI extends LightweightGuiDescription {
         fullButton.setAlignment(HorizontalAlignment.CENTER);
         fullButton.setIcon(new ItemIcon(new ItemStack(Items.DIAMOND)));
 
-        root.add(fullButton, 1, 9, 4, 20);
-        root.add(anonymousButton, 6, 9, 4, 20);
-        root.add(noneButton, 11, 9, 4, 20);
+        root.add(fullButton, 1, 9, 4, 1);
+        root.add(anonymousButton, 6, 9, 4, 1);
+        root.add(noneButton, 11, 9, 4, 1);
 
     }
 
