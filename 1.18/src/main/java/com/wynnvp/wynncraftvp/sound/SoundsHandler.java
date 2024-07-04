8bc9b4408465c392e44936ffc59ec8ed6680b0cf
kmaxii <esst936@gmail.com>
2024-07-04 15:49:56 +0200

    Cleaned up SoundsHandler

--- 1.18/src/main/java/com/wynnvp/wynncraftvp/sound/SoundsHandler.java
+++ 1.18/src/main/java/com/wynnvp/wynncraftvp/sound/SoundsHandler.java
@@ -2,29 +2,22 @@ package com.wynnvp.wynncraftvp.sound;
 
 import com.wynnvp.wynncraftvp.ModCore;
 import com.wynnvp.wynncraftvp.sound.line.LineData;
-
-
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
 
 import static com.wynnvp.wynncraftvp.utils.LineFormatter.formatToLineData;
 
 public class SoundsHandler {
 
-    //private final List<SoundObject> sounds;
     private final HashMap<String, SoundObject> sounds;
-    private final Set<String> npcNames;
 
     public SoundsHandler() {
         sounds = new HashMap<>();
-        npcNames = new HashSet<>();
 
         Sounds.register(this);
     }
@@ -35,8 +28,6 @@ public class SoundsHandler {
         SoundEvent se = SoundEvent.createVariableRangeEvent(id);
 
         Registry.register(BuiltInRegistries.SOUND_EVENT, id, se);
-
-
 
         return se;
     }
@@ -50,36 +41,25 @@ public class SoundsHandler {
      *                    otherwise it will move with the ArmorStand
      */
     public void addSound(String message, String id, boolean movingSound) {
-
         addSound(message, id, movingSound, null);
     }
 
     public void addSound(String message, String id, boolean movingSound, Vector3 position) {
-
         addSound(message, id, movingSound, position, 0);
     }
 
     public void addSound(String message, String id, boolean movingSound, int fallOff) {
-
-        LineData lineData = formatToLineData(message);
-        npcNames.add(lineData.getNPCName());
-        message = lineData.getSoundLine();
-        sounds.put(message, new SoundObject(lineData.getNPCName(), id, new CustomSoundClass(registerSound(id), movingSound), null, fallOff));
+        addSound(message, id, movingSound, null, fallOff);
     }
 
 
-    //If position is 0 null use default. If falloff is 0 use default
     public void addSound(String message, String id, boolean movingSound, Vector3 position, int fallOff) {
 
         LineData lineData = formatToLineData(message);
-        npcNames.add(lineData.getNPCName());
         message = lineData.getSoundLine();
         sounds.put(message, new SoundObject(lineData.getNPCName(), id, new CustomSoundClass(registerSound(id), movingSound), position, fallOff));
     }
 
-    public boolean containsName(String rawName) {
-        return npcNames.contains(rawName);
-    }
 
     public Optional<SoundObject> get(String message) {
         return Optional.ofNullable(sounds.get(message));
