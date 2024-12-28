21806e672091b440c243007116d50ff7f876cd48
kmaxii <kmaxii@users.noreply.github.com>
2024-12-28 17:32:28 +0000

    ci: spotless formatting

--- src/main/java/com/wynnvp/wynncraftvp/sound/downloader/AudioDownloader.java
+++ src/main/java/com/wynnvp/wynncraftvp/sound/downloader/AudioDownloader.java
@@ -33,8 +33,7 @@ public class AudioDownloader {
 
     private final String audioDir;
     private static final String METADATA_FILE = "audio_metadata.json";
-    private static final String BASE_URL =
-            "https://cdn.jsdelivr.net/gh/Team-VoW/WynncraftVoiceProject@main/sounds/";
+    private static final String BASE_URL = "https://cdn.jsdelivr.net/gh/Team-VoW/WynncraftVoiceProject@main/sounds/";
     private static final String AUDIO_MANIFEST =
             "https://cdn.jsdelivr.net/gh/Team-VoW/WynncraftVoiceProject@main/audio_manifest.json";
 
