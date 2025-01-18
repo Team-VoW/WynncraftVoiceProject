c1465f6e4b79e7121a18181dcd9fe814bfaf24c3
kmaxi <esst936@gmail.com>
2025-01-18 12:06:35 +0100

    Changed url to grab the sounds from just /sounds which is where they will be after merge

--- src/main/java/com/wynnvp/wynncraftvp/sound/downloader/AudioDownloader.java
+++ src/main/java/com/wynnvp/wynncraftvp/sound/downloader/AudioDownloader.java
@@ -37,7 +37,7 @@ public class AudioDownloader {
 
     private final String audioDir;
     private static final String BASE_URL =
-            "https://cdn.jsdelivr.net/gh/Team-VoW/WynncraftVoiceProject@main/src/main/resources/assets/wynnvp/sounds/";
+            "https://cdn.jsdelivr.net/gh/Team-VoW/WynncraftVoiceProject@main/sounds/";
     private static final String AUDIO_MANIFEST =
             "https://cdn.jsdelivr.net/gh/Team-VoW/WynncraftVoiceProject@main/audio_manifest.json";
 
