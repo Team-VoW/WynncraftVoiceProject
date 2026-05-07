49574d8dc5efb6e885d0745092619d0bb86e84cc
kmaxi <esst936@gmail.com>
2026-05-07 21:33:57 +0200

    fix(cloth-config): replace deprecated AutoConfig.getConfigScreen with AutoConfigClient

--- src/main/java/com/wynnvp/wynncraftvp/config/ModMenuIntegration.java
+++ src/main/java/com/wynnvp/wynncraftvp/config/ModMenuIntegration.java
@@ -1,16 +1,17 @@
 /*
- * Copyright © Team-VoW 2024.
+ * Copyright © Team-VoW 2024-2026.
  * This file is released under AGPLv3. See LICENSE for full license details.
  */
 package com.wynnvp.wynncraftvp.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(VOWAutoConfig.class, parent).get();
+        return parent ->
+                AutoConfigClient.getConfigScreen(VOWAutoConfig.class, parent).get();
     }
 }
